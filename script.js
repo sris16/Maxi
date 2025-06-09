8a6551cc0e055598658dd615924d9676affbbe69
sris16 <srisakthi7890@gmail.com>
2025-06-09 09:35:00 +0530

    Master

---
+++ script.js
@@ -0,0 +1,15 @@
+let i=0
+do{
+    i++
+    console.log(i);
+}while(i<5)
+    function greet(name , age)
+    {
+        return `Hello Mr ${name} is ${age} yaers old`
+    }
+    console.log(greet("Srisakthi",18));
+    function add(a,b)
+    {
+        return a+b
+    }
+    console.log(add(5,10))
